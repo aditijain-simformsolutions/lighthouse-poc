6b7db27f62a5875876e6745977df209440d1316f
Aditi jain <aditi.j@simformsolutions.com>
2021-03-22 11:24:19 +0530

    changes in json result

--- app.js
+++ app.js
@@ -16,7 +16,21 @@ app.post('/', async function (req, res) {
             const runnerResult = await lighthouse(req.body.url, options);
             await chrome.kill();
             console.log('Report is done for', runnerResult.lhr.finalUrl);
-            res.send(runnerResult.report);
+            if(req.body.type === 'json'){
+                let obj = {
+                    "first-contentful-paint": runnerResult.lhr.audits['first-contentful-paint'],
+                    "largest-contentful-paint": runnerResult.lhr.audits['largest-contentful-paint'],
+                    "first-meaningful-paint": runnerResult.lhr.audits['first-meaningful-paint'],
+                    "speed-index": runnerResult.lhr.audits['speed-index'],
+                    "cumulative-layout-shift": runnerResult.lhr.audits['cumulative-layout-shift'],
+                    "interactive": runnerResult.lhr.audits['interactive']  
+                }
+                res.send(obj);
+            }else{
+                //  fs.writeFileSync('lhreport.html', runnerResult.report);
+                //  res.sendFile(path.join(__dirname + '/lhreport.html'));
+                res.send(runnerResult.report);
+            }
         }else{
             res.send("Please pass url and type")
         }
